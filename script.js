51b3bc3a9c1f53602a9929625b0a2a3629eb6ec5
Vanilama Modded <https://github.com/Vanilama-modded/>
2026-02-12 18:25:15 +0100

    updated js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const headerTitle = document.getElementById("track-title");
 const headerArtist = document.getElementById("track-artist");
 
-// ref: https://github.com/rocksdanister/audio-visualizer-wallpaper/blob/main/src/Music%20Tunnel/js/script.js 
-
 function livelyCurrentTrack(data) {
   let obj = JSON.parse(data);
   
@@ -10,19 +8,11 @@ function livelyCurrentTrack(data) {
     headerTitle.innerText = obj.Title;
     headerTitle.href = `https://www.google.com/search?q=${encodeURIComponent(obj.Title + " song")}`;
     headerTitle.style.pointerEvents = "auto";
-
+    
     headerArtist.innerText = obj.Artist;
     headerArtist.href = `https://www.google.com/search?q=${encodeURIComponent(obj.Artist + " artist")}`;
     headerArtist.style.pointerEvents = "auto";
-    
-    console.log("Title:", obj.Title);
-    console.log("Artist:", obj.Artist);
-    
-    if (obj.Thumbnail != null) {
-      console.log("Thumbnail:", obj.Thumbnail);
-    }
   } else {
-    // clear/disable when no music is playing
     headerTitle.innerText = "No track";
     headerTitle.href = "#";
     headerTitle.style.pointerEvents = "none";
@@ -31,4 +21,13 @@ function livelyCurrentTrack(data) {
     headerArtist.href = "#";
     headerArtist.style.pointerEvents = "none";
   }
+}
+
+function livelyAudioListener(array) {
+}
+
+function livelyPropertyChanged(name, val) {
+}
+
+function livelySystemInformation(data) {
 }
